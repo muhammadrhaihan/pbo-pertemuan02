c98027c05970b4695862df5ce115aad3f6791e06
muhammadrhaihan <81276662+muhammadrhaihan@users.noreply.github.com>
2021-04-05 02:03:42 +0800

    Update InputOutput

--- InputOutput.java
+++ InputOutput.java
@@ -4,8 +4,20 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
+  int jumlahSKS;
+  double IPK;
+
   System.out.print("Masukan nama anda : ");
   nama = scanner.nextLine();
   System.out.println("Hai, "+nama);
+
+  System.out.print("Masukan Jumlah SKS yang sudah ditempuh anda : ");
+  jumlahSKS = scanner.nextInt();
+
+  System.out.print("Masukan IPK Sementara : ");
+  IPK = scanner.nextDouble();
+
+  double jumlah = IPK * jumlahSKS;
+  System.out.print("Total Nilai = "+jumlah);
  }
 }
