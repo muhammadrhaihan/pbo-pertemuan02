e1251df807c29cfd43aff03f1bc2274390495305
muhammadrhaihan <81276662+muhammadrhaihan@users.noreply.github.com>
2021-04-05 19:39:32 +0800

    Perbaikan

--- InputOutput.java
+++ InputOutput.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
 public class InputOutput{
- public static void main(String [] args){
-  Scanner scanner = new Scanner(System.in);
-  String nama;
-  int jumlahSKS;
-  double IPK;
+ public static void main(String[]args){
+  
+   String nama;
+   int jumlahSks;
+   double IPK;
+  
+   Scanner scanner = new Scanner(System.in);
+ 
+   System.out.print("Input nama : ");
+   nama = scanner.nextLine();
 
-  System.out.print("Masukan nama anda : ");
-  nama = scanner.nextLine();
-  System.out.println("Hai, "+nama);
+   System.out.print("Input usia : ");
+   usia = scanner.nextInt();
 
-  System.out.print("Masukan Jumlah SKS yang sudah ditempuh anda : ");
-  jumlahSKS = scanner.nextInt();
+   System.out.print("Input IPK : ");
+   IPK = scanner.nextDouble();
 
-  System.out.print("Masukan IPK Sementara : ");
-  IPK = scanner.nextDouble();
-
-  double jumlah = IPK * jumlahSKS;
-  System.out.print("Total Nilai = "+jumlah);
+   System.out.println("Hai " + nama);
+   System.out.println("usia : " + usia);
+   System.out.println("IPK : " + IPK);
  }
 }
