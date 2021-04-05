d717f1ba3b02a6d2bcca88b1aec7717edfed6849
muhammadrhaihan <81276662+muhammadrhaihan@users.noreply.github.com>
2021-04-05 20:16:51 +0800

    Compile & Run

--- InputOutput.java
+++ InputOutput.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class InputOutput{
-<<<<<<< HEAD
  public static void main(String[]args){
   
    String nama;
@@ -12,25 +11,21 @@ public class InputOutput{
  
    System.out.print("Input nama : ");
    nama = scanner.nextLine();
-=======
- public static void main(String [] args){
-  Scanner scanner = new Scanner(System.in);
-  String nama;
-  int jumlahSKS;
-  double IPK;
->>>>>>> parent of e1251df (Perbaikan)
 
-  System.out.print("Masukan nama anda : ");
-  nama = scanner.nextLine();
-  System.out.println("Hai, "+nama);
+   System.out.print("Masukan nama anda : ");
+   nama = scanner.nextLine();
+   System.out.println("Hai, "+nama);
 
-  System.out.print("Masukan Jumlah SKS yang sudah ditempuh anda : ");
-  jumlahSKS = scanner.nextInt();
+   System.out.print("Input usia : ");
+   usia = scanner.nextInt();
 
-  System.out.print("Masukan IPK Sementara : ");
-  IPK = scanner.nextDouble();
+   System.out.print("Input IPK : ");
+   IPK = scanner.nextDouble();
 
-  double jumlah = IPK * jumlahSKS;
-  System.out.print("Total Nilai = "+jumlah);
+   System.out.println("Hai " + nama);
+
+   System.out.println("usia : " + usia);
+
+   System.out.println("IPK : " + IPK);
  }
 }
